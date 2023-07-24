fcf4a6311e42356842e7b8c7a01ee253d095f2e7
Cooper <cooper.softdev@gmail.com>
2023-07-24 14:44:25 -0800

    init commit for Spring and gitignore

---
+++ src/test/java/com/songr/songrapp/SongrappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songr.songrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongrappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
